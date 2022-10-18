26ae7a0170b1746f39bf6b54becea1f6cdcd88e3
Yann Klein <yann.klein@me.com>
2022-10-18 21:31:08 +0900

    base repo

--- Gemfile
+++ Gemfile
@@ -2,14 +2,14 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'sinatra-contrib'
-gem 'sinatra-activerecord'
+# gem 'sinatra-activerecord'
 gem 'rake'
-gem 'pg'
+# gem 'pg'
 gem 'rspec'
 gem 'rubocop'
 gem 'pry'
 gem 'faker'
 
-group :development do
-  gem 'sqlite3'
-end
+# group :development do
+#   gem 'sqlite3'
+# end
--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
-require 'sinatra/activerecord'
-require_relative 'config/application'
+# require 'sinatra/activerecord'
+# require_relative 'config/application'
 
 get '/' do
   @hello = 'Hi there!'
@@ -18,8 +18,8 @@ end
 
 # DO NOT CHANGE BELOW LINES
 # Some configuration for Sinatra to be hosted and operational on Heroku
-after do
-  # Close the connection after the request is done so that we don't
-  # deplete the ActiveRecord connection pool.
-  ActiveRecord::Base.connection.close
-end
+# after do
+#   # Close the connection after the request is done so that we don't
+#   # deplete the ActiveRecord connection pool.
+#   ActiveRecord::Base.connection.close
+# end
